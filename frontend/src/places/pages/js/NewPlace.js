58dcf173e4f1e75c157ac749746ecb0f645a4a30
J0E-E <joeyiglesias83@gmail.com>
2023-12-18 20:16:08 -0500

    add validators and manage input component state

--- frontend/src/places/pages/js/NewPlace.js
+++ frontend/src/places/pages/js/NewPlace.js
@@ -2,11 +2,18 @@ import React from "react";
 import Input from "../../../shared/components/FormElements/js/Input";
 
 import "../scss/NewPlace.scss"
+import {VALIDATOR_REQUIRE} from "../../../shared/Utils/Validators";
 
 const NewPlace = () => {
     return (
         <form className="place-form">
-            <Input element="input" type="text" label="Title"/>
+            <Input
+                element="input"
+                type="text"
+                label="Title"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid title."
+            />
         </form>
     )
 };
